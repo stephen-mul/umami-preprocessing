d11ae118c24cbeea8303b7f7c56939e1f15610b3
Paul Philipp Gadow <paul.philipp.gadow@cern.ch>
2024-09-23 11:01:13 +0200

    update

---
+++ setup_conda.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# install mamba locally if it doesn't already exist
+CONDA_INSTALL=$PWD/conda/
+
+if [[ ! -d "${CONDA_INSTALL}" ]]; then
+  CONDA_REPOSITORY=https://github.com/conda-forge/miniforge/releases/latest/download
+  # installation for macOS (including support for M1 MacBooks)
+  if [[ $OSTYPE == 'darwin'* ]]; then
+    MAC_TYPE="$(uname -m)"
+    if [[ $MAC_TYPE == 'arm64' ]]; then
+      CONDA_INSTALLER="Mambaforge-MacOSX-arm64.sh"
+    else
+      CONDA_INSTALLER="Mambaforge-MacOSX-x86_64.sh"
+    fi
+  # installation for linux
+  elif [[ $OSTYPE == 'linux'* ]]; then
+    CONDA_INSTALLER="Mambaforge-Linux-x86_64.sh"
+  # other operating system not supported
+  else
+    echo "Operating system not supported. Setup not possible."
+    exit 1
+  fi
+  # install mamba to local directory
+  curl -L -O ${CONDA_REPOSITORY}/${CONDA_INSTALLER}
+  bash ${CONDA_INSTALLER} -b -p ${CONDA_INSTALL}
+  rm ${CONDA_INSTALLER}
+fi
+
+# set up conda environment
+source ${CONDA_INSTALL}/bin/activate
+conda create -n upp python=3.10
+conda activate upp
+
